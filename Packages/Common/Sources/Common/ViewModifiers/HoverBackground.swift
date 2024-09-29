54a9478134edd2e4273de08dc8e115e959bb4498
Brad Angliss <banglis1@jaguarlandrover.com>
2024-09-29 17:24:23 +0100

    feat: create common package with hoverBackground view modifier

---
+++ Packages/Common/Sources/Common/ViewModifiers/HoverBackground.swift
@@ -0,0 +1,29 @@
+//
+//  ClipHistoryApp.swift
+//  Common
+//
+//  Created by Brad Angliss on 29/09/2024.
+//
+
+import SwiftUI
+
+public extension View {
+    func hoverBackground(
+        isHovering: Binding<Bool>
+    ) -> some View {
+        self.modifier(HoverBackgroundViewModifier(isHovering: isHovering.wrappedValue))
+    }
+}
+
+private struct HoverBackgroundViewModifier: ViewModifier {
+    @State public var isHovering: Bool
+    
+    func body(content: Content) -> some View {
+        content
+            .background(isHovering ? .gray : .clear)
+            .clipShape(RoundedRectangle(cornerRadius: 5))
+            .onHover(perform: { hovering in
+                isHovering = hovering
+            })
+    }
+}
